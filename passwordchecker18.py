634b9d590f4114a2d7734019c8b4065891a559a0
Kinza shakil <creativivity.mind@gmail.com>
2025-03-05 20:52:17 +0500

    passwordchecker18.py

--- passwordchecker18.py
+++ passwordchecker18.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Password Strength Checker", page_icon=("🌘"), l
 st.markdown("""
 <style>
     .main {text-align: centre;}
-    .stTextInput {width: 50% !important; margin: auto;}
+    .stTextInput {width: 70% !important; margin: auto;}
     .stButton button {width: 70; background-color: lightblue; color: black; font-size: 18px;}
     .stButton button:hover {background-color: lightgray; color: black;}
 </style>
