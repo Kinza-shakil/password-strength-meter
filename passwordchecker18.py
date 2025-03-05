84c938f6288c328314bb739f6c307c09af95d80b
Kinza shakil <creativivity.mind@gmail.com>
2025-03-05 20:44:08 +0500

    passwordchecker18.py

--- passwordchecker18.py
+++ passwordchecker18.py
@@ -15,7 +15,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Page title and description
-st.title("🔐 Password Strength Generator")
+st.title("🔐 Password Strength Generator By Kinza")
 st.write("Enter your password below to check its security level. 🔍")
 
 # Function to check password strength
